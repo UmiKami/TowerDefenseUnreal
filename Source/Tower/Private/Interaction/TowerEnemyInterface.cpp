b56a6dd74b4046ba5e699b39a374bb3eaadd8cde
umikami <ggoku02@gmail.com>
2026-04-08 16:52:35 -0400

    Created interface just to check if actor is enemy without having to cast.

---
+++ Source/Tower/Private/Interaction/TowerEnemyInterface.cpp
@@ -0,0 +1,7 @@
+// Copyright UmiKami (Ernesto Gonzalez) © 2026
+
+
+#include "Interaction/TowerEnemyInterface.h"
+
+
+// Add default functionality here for any ITowerEnemyInterface functions that are not pure virtual.
---
+++ Source/Tower/Public/Interaction/TowerEnemyInterface.h
@@ -0,0 +1,25 @@
+// Copyright UmiKami (Ernesto Gonzalez) © 2026
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "TowerEnemyInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class UTowerEnemyInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class TOWER_API ITowerEnemyInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+};
