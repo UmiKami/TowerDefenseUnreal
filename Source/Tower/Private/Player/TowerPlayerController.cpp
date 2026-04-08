9b2d854bb346a83b5bfbfb665df22ff9b1cce610
umikami <ggoku02@gmail.com>
2026-04-08 15:17:08 -0400

    Added setup for default input mapping context

--- Source/Tower/Private/Player/TowerPlayerController.cpp
+++ Source/Tower/Private/Player/TowerPlayerController.cpp
@@ -1,4 +1,24 @@
 // Copyright UmiKami (Ernesto Gonzalez) © 2026
 
 #include "Player/TowerPlayerController.h"
+#include "EnhancedInputSubsystems.h"
 
+ATowerPlayerController::ATowerPlayerController()
+{
+	bShowMouseCursor = true;
+}
+
+void ATowerPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	checkf(DefaultMappingContext, TEXT("Default input mapping context not set in Tower Player Controller."));
+	
+	if (IsLocalPlayerController())
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
+}
--- Source/Tower/Public/Player/TowerPlayerController.h
+++ Source/Tower/Public/Player/TowerPlayerController.h
@@ -6,12 +6,26 @@
 #include "GameFramework/PlayerController.h"
 #include "TowerPlayerController.generated.h"
 
+class UInputMappingContext
+
 /**
+ * @brief Custom player controller for input setup
  * @ingroup Tower
  */
 UCLASS()
 class TOWER_API ATowerPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+	/**
+	 * @brief Sets mouse cursor visibility to true.
+	 */
+	ATowerPlayerController();
 	
+protected:
+	virtual void SetupInputComponent() override;
+	
+	UPROPERTY(EditAnywhere, Category ="Input|Input Mappings")
+	TObjectPtr<UInputMappingContext> DefaultMappingContext;
 };
