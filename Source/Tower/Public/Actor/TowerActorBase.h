e2276754953d598bd17730798616c3ee5283c199
umikami <ggoku02@gmail.com>
2026-04-09 08:35:58 -0400

    added Global class info ptr

--- Source/Tower/Public/Actor/TowerActorBase.h
+++ Source/Tower/Public/Actor/TowerActorBase.h
@@ -54,4 +54,6 @@ protected:
 	UFUNCTION()
 	void OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
 	
+private:
+	TObjectPtr<FTowerClasDefaultInfo> TowerClasDefaultInfo;
 };
