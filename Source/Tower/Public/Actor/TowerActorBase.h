a02374e87f48ff232c82197067160aa907613bf6
umikami <ggoku02@gmail.com>
2026-04-09 08:48:42 -0400

    TObjectPtr does not work with struct, replaced with raw pointer

--- Source/Tower/Public/Actor/TowerActorBase.h
+++ Source/Tower/Public/Actor/TowerActorBase.h
@@ -55,5 +55,5 @@ protected:
 	void OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
 	
 private:
-	TObjectPtr<FTowerClasDefaultInfo> TowerClasDefaultInfo;
+	FTowerClasDefaultInfo* TowerClasDefaultInfo;
 };
