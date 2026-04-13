08634c3a2645e2ccd65a878a5c4a04fe5e944e85
umikami <ggoku02@gmail.com>
2026-04-13 13:14:39 -0400

    Minor fix for character to be oriented in the direction it is going at

--- Source/Tower/Private/Actor/Enemy/TowerEnemyPawn.cpp
+++ Source/Tower/Private/Actor/Enemy/TowerEnemyPawn.cpp
@@ -68,6 +68,9 @@ void ATowerEnemyPawn::MoveActorAlongSpline()
 
 	FVector Direction = (TargetLocation - GetActorLocation()).GetSafeNormal();
 	Direction.Z = 0.f;
+	
+	SetActorRotation(Direction.Rotation());
+	
 	AddMovementInput(Direction, 1.f);
 }
 
