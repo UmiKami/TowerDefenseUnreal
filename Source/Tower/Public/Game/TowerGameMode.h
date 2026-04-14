73d307e284adbf2bd6fc67f6de55ee1fd0851103
umikami <ggoku02@gmail.com>
2026-04-14 16:51:53 -0400

    Added wave management logic

--- Source/Tower/Public/Game/TowerGameMode.h
+++ Source/Tower/Public/Game/TowerGameMode.h
@@ -6,6 +6,7 @@
 #include "GameFramework/GameMode.h"
 #include "TowerGameMode.generated.h"
 
+class ATowerGameState;
 /**
  * @ingroup Tower
  */
@@ -14,4 +15,28 @@ class TOWER_API ATowerGameMode : public AGameMode
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void BeginPlay() override;
+	
+	virtual void Tick(float DeltaSeconds) override;
+	
+protected:
+	UFUNCTION(BlueprintImplementableEvent)
+	void SpawnEnemy();
+	
+	UFUNCTION(BlueprintCallable)
+	void StartNextWave();
+	
+	FTimerHandle EnemySpawnTimer;
+	
+	// TODO Consider evaluating this on a curve so there is less delays between waves the higher up we are
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly);
+	int DelayBeforeNextWave = 5.f;
+
+	UPROPERTY(BlueprintReadWrite)
+	TObjectPtr<ATowerGameState> TowerGameState;
+	
+private:
+	UFUNCTION(BlueprintCallable)
+	void StopSpawningEnemy(); 
 };
