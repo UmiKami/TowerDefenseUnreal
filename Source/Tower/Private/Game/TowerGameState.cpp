8ed3314b12c36820b766ec2e3888708064f46cb8
umikami <ggoku02@gmail.com>
2026-04-17 11:42:51 -0400

    Added GameTime and Game State to only capture time during playing state.

--- Source/Tower/Private/Game/TowerGameState.cpp
+++ Source/Tower/Private/Game/TowerGameState.cpp
@@ -3,3 +3,13 @@
 
 #include "Game/TowerGameState.h"
 
+#include "GameFramework/GameMode.h"
+
+void ATowerGameState::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	
+	if (State != Playing) return;
+	
+	GameTime+=DeltaSeconds;
+}
