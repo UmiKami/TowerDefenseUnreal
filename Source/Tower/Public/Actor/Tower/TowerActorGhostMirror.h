a51a884d9e4c6310833b5633ec723c783d47a3c8
UmiKami <ggoku02@gmail.com>
2026-04-30 16:58:26 -0400

    Added simple actor act as the Tower ghost before it is placed

---
+++ Source/Tower/Public/Actor/Tower/TowerActorGhostMirror.h
@@ -0,0 +1,19 @@
+﻿// Copyright UmiKami (Ernesto Gonzalez) © 2026
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TowerActorGhostMirror.generated.h"
+
+UCLASS()
+class TOWER_API ATowerActorGhostMirror : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ATowerActorGhostMirror();
+
+	UPROPERTY(EditAnywhere)
+	USkeletalMeshComponent* Mesh;
+};
