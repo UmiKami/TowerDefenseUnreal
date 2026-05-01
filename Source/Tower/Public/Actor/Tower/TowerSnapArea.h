0111a58986a10b05b2291f0ac382ae0c9981d7f8
UmiKami <ggoku02@gmail.com>
2026-04-30 22:26:09 -0400

    Added basic snap area

---
+++ Source/Tower/Public/Actor/Tower/TowerSnapArea.h
@@ -0,0 +1,24 @@
+﻿// Copyright UmiKami (Ernesto Gonzalez) © 2026
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "TowerSnapArea.generated.h"
+
+class UBoxComponent;
+/**
+ * 
+ */
+UCLASS()
+class TOWER_API ATowerSnapArea : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	ATowerSnapArea();
+	
+protected:
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UStaticMeshComponent> SnapAreaRecognitionRangeMesh;
+};
