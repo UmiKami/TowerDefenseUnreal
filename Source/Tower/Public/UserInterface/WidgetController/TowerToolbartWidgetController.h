121421e189c914c9e7a562cdef51a1afb7b793b9
UmiKami <ggoku02@gmail.com>
2026-05-03 19:14:26 -0400

    When a tower is placed, player controller withraws coins from player state.

---
+++ Source/Tower/Public/UserInterface/WidgetController/TowerToolbartWidgetController.h
@@ -0,0 +1,21 @@
+﻿// Copyright UmiKami (Ernesto Gonzalez) © 2026
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TowerWidgetController.h"
+#include "TowerToolbartWidgetController.generated.h"
+
+class UTowerClassInfo;
+/**
+ * 
+ */
+UCLASS()
+class TOWER_API UTowerToolbartWidgetController : public UTowerWidgetController
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TObjectPtr<UTowerClassInfo> TowerClassInfoDataAsset;
+};
